2d65ef63744b59f82bee0b6a90bf5086dd5f516a
Mark Janssen <mark@praseodym.net>
2016-01-12 13:43:02 +0100

    Convert all problems at once

--- rers2015/traceconvert/traceconvert.py
+++ rers2015/traceconvert/traceconvert.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import string
 
@@ -8,22 +10,27 @@ def alphabet_index(char):
     except ValueError:
         return -1
 
-problem = 6
 
-input_file = "../Problem{:d}/Problem{:d}-Traces.txt".format(problem, problem)
-output_dir = "Problem{:d}-Traces".format(problem)
-traces = open(input_file)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+def write_traces(problem):
+    input_file = "../Problem{:d}/Problem{:d}-Traces.txt".format(problem, problem)
+    output_dir = "Problem{:d}-Traces".format(problem)
+    traces = open(input_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-for trace in traces:
-    num = trace[:trace.find("#")].zfill(3)
-    symbols = trace[trace.find("# ") + 2:].split(",")
-    numeric = list()
-    for symbol in symbols:
-        index = alphabet_index(symbol)
-        if index > 0:
-            numeric.append(str(index))
-    print("trace {:s}".format(num))
-    output_file = open(output_dir + '/' + num + '.txt', 'w')
-    output_file.write("\n".join(numeric))
+    for trace in traces:
+        num = trace[:trace.find("#")].zfill(3)
+        symbols = trace[trace.find("# ") + 2:].split(",")
+        numeric = list()
+        for symbol in symbols:
+            index = alphabet_index(symbol)
+            if index > 0:
+                numeric.append(str(index))
+        print("\tTrace {:s}".format(num))
+        output_file = open(output_dir + '/' + num + '.txt', 'w')
+        output_file.write("\n".join(numeric))
+
+
+for problem in range(4, 7):
+    print("Problem {:d}".format(problem))
+    write_traces(problem)
