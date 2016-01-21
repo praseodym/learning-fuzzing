7f2521c57aed3626a3bdf0c495406b8413cad8bb
Mark Janssen <mark@praseodym.net>
2016-01-21 18:08:52 +0100

    Include third state in simple state machine

--- simpletarget/statemachine.c
+++ simpletarget/statemachine.c
@@ -10,7 +10,7 @@ int current_state = 0;
 char step(char input) {
   switch (current_state) {
     case 0:
-      switch(input) {
+      switch (input) {
         case 'A':
           current_state = 1;
           return 'X';
@@ -23,9 +23,9 @@ char step(char input) {
           invalid_input();
       }
     case 1:
-      switch(input) {
+      switch (input) {
         case 'A':
-          current_state = 2;
+          current_state = 3;
           return 'Z';
         case 'B':
           return 'Y';
@@ -35,7 +35,7 @@ char step(char input) {
           invalid_input();
       }
     case 2:
-      switch(input) {
+      switch (input) {
         case 'A':
           return 'Z';
         case 'B':
@@ -46,8 +46,23 @@ char step(char input) {
         default:
           invalid_input();
       }
+    case 3:
+      switch (input) {
+        case 'A':
+          // Some side effect here
+          return 'Z';
+        case 'B':
+          return 'Y';
+        case 'C':
+          current_state = 0;
+          return 'Z';
+        default:
+          invalid_input();
+      }
   }
+  return 0;
 }
+
 int main() {
 #ifdef __AFL_HAVE_MANUAL_CONTROL
   __AFL_INIT();
