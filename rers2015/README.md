26e6616809e1a1ea61922f0b86406be02910b102
Mark Janssen <mark@praseodym.net>
2016-04-11 12:33:47 +0200

    Better RERS2015 problems build

--- rers2015/README.md
+++ rers2015/README.md
@@ -1,10 +1,9 @@
 # RERS 2015 Problems
 
-Dependencies: `apt-get install cmake ninja-build`
+Dependencies: `apt-get install build-essential cmake ninja-build`
 
-Building (feel free to set `CC=` to the compiler of your choice):
+Building:
 ```shell
 mkdir -p build && cd build
-CC=clang cmake -GNinja .. && cmake --build .
+cmake -GNinja .. && cmake --build .
 ```
-
--- rers2015/assert.h
+++ rers2015/assert.h
@@ -1,18 +1,8 @@
-//
-// Created by mark on 1/6/16.
-//
-
-#ifndef RERSPROBLEM1_ASSERT_H
-#define RERSPROBLEM1_ASSERT_H
+#ifndef RERSPROBLEMS_ASSERT_H
+#define RERSPROBLEMS_ASSERT_H
 
 #include <sys/cdefs.h>
 #include <stdlib.h>
-
-#if defined __cplusplus && __GNUC_PREREQ (2,95)
-# define __ASSERT_VOID_CAST static_cast<void>
-#else
-# define __ASSERT_VOID_CAST (void)
-#endif
 
 extern void __assert_fail (const char *__assertion, const char *__file,
                            unsigned int __line, const char *__function) {
@@ -21,19 +11,9 @@ extern void __assert_fail (const char *__assertion, const char *__file,
   exit(3);
 }
 
-# define assert(expr)							\
-  ((expr)								\
-   ? __ASSERT_VOID_CAST (0)						\
-   : __assert_fail (__STRING(expr), __FILE__, __LINE__, __ASSERT_FUNCTION))
+# define assert(expr) \
+  ((expr) \
+   ? (void) (0) \
+   : __assert_fail (__STRING(expr), __FILE__, __LINE__, 0))
 
-# if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
-#   define __ASSERT_FUNCTION	__PRETTY_FUNCTION__
-# else
-#  if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
-#   define __ASSERT_FUNCTION	__func__
-#  else
-#   define __ASSERT_FUNCTION	((const char *) 0)
-#  endif
-# endif
-
-#endif //RERSPROBLEM1_ASSERT_H
+#endif //RERSPROBLEMS_ASSERT_H
